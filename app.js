78ef6cd03c8355f54d0903ec82329784dbec28d0
James Maveety <jimbobthedrummer@gmail.com>
2015-08-05 12:10:54 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,56 @@
+
+var app = require('../app');
+var debug = require('debug')('server');
+var http = require('http');
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+/* Using debug instead of console.log provides some convenient
+*  parcellation of log messages and removes the need to remove log
+*  statements before deployment. Activate log messages when starting up
+* server by running DEBUG=server npm start
+*/
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+
