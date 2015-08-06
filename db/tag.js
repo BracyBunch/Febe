bb8bcdb1877e144df411a157c44764c55ea30874
eatpie75 <eatpie75@gmail.com>
2015-08-05 18:53:54 -0700

    [fix] move comments

--- db/tag.js
+++ db/tag.js
@@ -3,6 +3,17 @@ var seraph = require('seraph');
 var model = require('seraph-model');
 var db = seraph();
 
+/*
+  get list of skill for project:
+    match (skills)<-[:skill]-(project:Project) where id(project)={id} return skills
+
+  get list of projects for skill:
+    match (projects:Project)-[:skill]-(tag:Tag) where id(tag)={id} return projects
+
+  autocomplete tag:
+    match (tags:Tag) where tags.name=~'{text}.*' return tags
+ */
+
 var Tag = model(db, 'Tag');
 Tag.schema = {
   'kind': {'type': String, 'required': true, 'enum': ['skill', 'cause']},
