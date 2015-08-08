98b04178ba3b37cc4b2864527d0904396825a396
Yoshio Varney <yoshio.varney@gmail.com>
2015-08-08 15:40:18 -0700

    [feat] finished preliminary oauth signup view

--- public/components/signupOAuth.jsx
+++ public/components/signupOAuth.jsx
@@ -2,12 +2,12 @@ var React = require('react');
 var Router = require('react-router');
 var Link = Router.Link;
 
-
+// need form validation
 module.exports = React.createClass({
 	render: function() {
     return (
 	    <div className="signupForm">
-	      <form>
+	      <form className="form-inline">
 		      Developer Signup <br />
 		      <Link to="#">
 		        <img className="oauthPic img-rounded" src="assets/img/linkedinAuth.png" />
@@ -19,11 +19,12 @@ module.exports = React.createClass({
 		        <img className="oauthPic img-rounded" src="assets/img/facebookAuth.png" />
 		      </Link> <br />
 		      Or <br />
-		      <input type="text" value="" placeholder="First Name"/>
-		      <input type="text" value="" placeholder="Last Name"/> <br />
-		      <input type="text" value="" placeholder="Email Address" className="form-control"/> <br />
-		      <input type="text" value="" placeholder="Password"/>
-		      <input type="text" value="" placeholder="Confirm Password"/>
+		      <input type="text" value="" placeholder="First Name" className="form-control formbox" />
+		      <input type="text" value="" placeholder="Last Name" className="form-control formbox" /> <br />
+		      <input type="text" value="" placeholder="Email Address" className="form-control formbox" /> <br />
+		      <input type="text" value="" placeholder="Password" className="form-control formbox" />
+		      <input type="text" value="" placeholder="Confirm Password" className="form-control formbox" /> <br />
+		      Password must be more than 8 characters
 		    </form>  
 	    </div>
 	  )
