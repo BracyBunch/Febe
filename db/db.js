572a1d7b32697cd6cd54f94bb6ea354f3db9fc2c
eatpie75 <eatpie75@gmail.com>
2015-08-08 11:35:09 -0700

    [feat] Add db.has_rel

--- db/db.js
+++ db/db.js
@@ -16,4 +16,16 @@ var db = seraph(options);
 db.query = Promise.promisify(db.query);
 db.relate = Promise.promisify(db.relate);
 
+db.has_rel = function(start_label, start_id, rel_name, end_label, end_id) {
+  var query = [
+    'MATCH (start:' + start_label + ') WHERE id(start)={start_id}',
+    'MATCH (end:'+ end_label + ') WHERE id(end)={end_id} AND (start)-[:' + rel_name + ']->(end)',
+    'RETURN COUNT(end)>0 AS exists'
+  ].join(' ');
+
+  return db.query(query, {'start_id': start_id, 'rel_name': rel_name, 'end_id': end_id}).then(function(row) {
+    return row.exists;
+  });
+};
+
 module.exports = db;
