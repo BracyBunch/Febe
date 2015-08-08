cb065798145e529b0a4fdf71a10dffb6ffcef7a4
eatpie75 <eatpie75@gmail.com>
2015-08-08 10:04:32 -0700

    [feat] Add Tag.create and Tag.update

--- db/methods/tag.js
+++ db/methods/tag.js
@@ -1,5 +1,34 @@
-// var Promise = require('bluebird');
-var db = require('../');
-var Tag = db.Tag;
+var _ = require('lodash');
+var db = require('../db');
+var Tag = require('../models/tag');
 
-module.exports = {};
+/**
+ * Creates a new Tag
+ * @param  {Object} fields  Fields to set on Tag
+ * @return {Promise.<Tag>}
+ */
+var create = function(fields) {
+  return Tag.save(fields);
+};
+
+/**
+ * Update a Tag
+ * @param  {Integer} [id]     Id of the Tag to update, can be omitted if there is an id key in fields
+ * @param  {Object} fields    Fields to update
+ * @return {Promise.<User>}
+ */
+var update = function(id, fields) {
+  if (typeof id === 'object') {
+    fields = id;
+    id = fields.id;
+  }
+
+  return Tag.read(id).then(function(tag) {
+    return Tag.save(_.extend(tag, fields), {'id': id});
+  });
+};
+
+module.exports = {
+  'create': create,
+  'update': update
+};
