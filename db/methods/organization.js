76a6b187c89ad4c9926eed96e44f7cedd802a0a2
eatpie75 <eatpie75@gmail.com>
2015-08-07 19:13:40 -0700

    [docs] Add docs for Organization.create

--- db/methods/organization.js
+++ db/methods/organization.js
@@ -2,6 +2,12 @@ var Promise = require('bluebird');
 var db = require('../db');
 var Organization = require('../models/organization');
 
+/**
+ * Create and save a new Organization
+ * @param  {Object}        fields    Fields to create Organization with
+ * @param  {Integer|User}  owner     User object or id of the Organization owner
+ * @return {Promise.<Organization>}  The newly created Organization
+ */
 var create = function(fields, owner) {
   if (owner === undefined) return Promise.reject('Owner not given.');
 
