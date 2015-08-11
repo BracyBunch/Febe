a13d5869a6e7ae0035738e8b105b44a863465b2c
Ryan Jones <ryan.j.jones10@gmail.com>
2015-08-10 20:08:13 -0700

    [fix] Clean up singup forms

--- public/components/signupDev.jsx
+++ public/components/signupDev.jsx
@@ -3,14 +3,14 @@ var React = require('react');
 module.exports = React.createClass({
 	render: function() {
 		return (
-	    <form className="col-md-6 col-md-offset-3">
-	      <div className="form-group">
-			    <label for="techstrengths">Tech Strengths (optional)</label> <br />
-		      <input type="text" value="" id="techstrengths" placeholder="Angular.js, Node.js, Python, Databases, etc." className="form-control formbox" />
+	    <form>
+	      <div className="form-group techstrengths">
+			    <label htmlFor="techstrengths">Tech Strengths (optional)</label> 
+		      <input type="text" id="techstrengths" placeholder="Angular.js, Node.js, Python, Databases, etc." className="form-control" />
 		    </div>
-				<div className="form-group">
-			    <label for="linkss">Additional Links (optional)</label> <br />
-		      <input type="text" value="" id="links" placeholder="LinkedIn, Github, Angel List, Website, etc." className="form-control formbox" />
+				<div className="form-group techstrengths">
+			    <label htmlFor="links">Additional Links (optional)</label>
+		      <input type="text" id="links" placeholder="LinkedIn, Github, Angel List, Website, etc." className="form-control" />
 		    </div>
 		    <div className="signupCentered">
 			    <button className="btn signupBtn">Add +</button> <br />
--- public/components/signupOAuth.jsx
+++ public/components/signupOAuth.jsx
@@ -6,43 +6,39 @@ var Link = Router.Link;
 module.exports = React.createClass({
 	render: function() {
     return (
-	    <div className="col-md-6 col-md-offset-3">
-	      <form className="">
-
-	        <div className="signupCentered">
-			      <h3 className="">Developer Signup</h3>
-			      <div className="form-group">
-				      <Link to="#">
-				        <img className="oauthPic img-rounded" src="assets/img/linkedinAuth.png" />
-				      </Link>
-				      <Link to="#">
-				        <img className="oauthPic img-rounded" src="assets/img/githubAuth.png" />
-				      </Link>
-				      <Link to="#">
-				        <img className="oauthPic img-rounded" src="assets/img/facebookAuth.png" />
-				      </Link>
-				    </div>
-			      <h5>Or</h5>
+	    <div>
+        <div className="signupCentered">
+		      <h3>Developer Signup</h3>
+		      <div className="btn-group">
+			      <Link to="#">
+			      	<img className="oauthPic img-rounded" src="assets/img/linkedinAuth.png" />
+			      </Link>
+			      <Link to="#">
+			        <img className="oauthPic img-rounded" src="assets/img/githubAuth.png" />
+			      </Link>
+			      <Link to="#">
+			        <img className="oauthPic img-rounded" src="assets/img/facebookAuth.png" />
+			      </Link>
 			    </div>
-  				
-		      <div className="form-group">
-			      <input type="text" value="" placeholder="First Name" className="form-inline formbox" />
-			      <input type="text" value="" placeholder="Last Name" className="form-inline formbox" />
-			    </div>
-
-			    <div>
-			      <input type="text" value="" placeholder="Email Address" className="form-control" />
-			    </div>
-
-		      <div className="form-group">
-			      <input type="text" value="" placeholder="Password" className="form-inline formbox" />
-			      <input type="text" value="" placeholder="Confirm Password" className="form-inline <formbox></formbox>" />
-			    </div>
-
-		      <h5 className="signupCentered">Password must be more than 8 characters</h5>
-
-		    </form>  
-	    </div>
+		      <h5>Or</h5>
+		    </div>
+		    <form className="form-inline names">
+		    	<div className="form-group">
+    				<input type="text" className="form-control firstName" placeholder="First Name"/>
+    				<input type="text" className="form-control lastName" placeholder="Last Name"/>
+		    	</div>
+		    </form>
+		    <div className="form-group">
+	      	<input type="email" className="form-control emailFill" placeholder="Email Address"/>
+	      </div>
+	      <form className="form-inline passwords">
+		    	<div className="form-group">
+    				<input type="password" className="form-control password" placeholder="Password"/>
+    				<input type="password" className="form-control" placeholder="Confirm Password"/>
+		    	</div>
+		    </form>
+	      <h5 className="signupCentered">Password must be more than 8 characters</h5>  
+    	</div>
 	  )
 	}
 });
