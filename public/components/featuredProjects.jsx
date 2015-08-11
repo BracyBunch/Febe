1ae3b2b1d937bcc37902019376d20edacd25e57d
Ryan Jones <ryan.j.jones10@gmail.com>
2015-08-10 18:55:24 -0700

    [feat] Update homepage

---
+++ public/components/featuredProjects.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <div className="featured">featured Projects & Organizations</div>
+      </div>
+    )
+  }
+})
---
+++ public/components/help.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function(){
+    return (
+      <div className="help">
+        <div> Want to help out at Good In This World? </div> 
+        <button className="btn btn-primary" type="submit">Contact Us</button>
+      </div>
+    )
+  }
+})
