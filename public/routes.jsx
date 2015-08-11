ceab40a730ca86ddad39f68de21f5231f4728307
Ryan Jones <ryan.j.jones10@gmail.com>
2015-08-11 12:21:19 -0700

    [feat]added auth checking for sign up and file for dashboard

--- public/routes.jsx
+++ public/routes.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactRouter = require('react-router');
 //object that tells the router how we will keep track of where the user is
-var HashHistory = require('react-router/lib/HashHistory');
+var HashHistory = require('react-router/lib/HashHistory').default;
 //what to show on the page at any given time
 var Router = ReactRouter.Router;
 //object used to configure the router
@@ -9,6 +9,9 @@ var Route = ReactRouter.Route;
 var Main = require('./components/main');
 var Signupdev = require('./components/signupDevView');
 var Signuporg = require('./components/signupOrgView');
+var Termsofuse = require('./components/termsOfUse');
+var Privacypolicy = require('./components/privacyPolicy');
+var Dashboard = require('./components/dashboard');
 
 
 module.exports = (
@@ -16,6 +19,9 @@ module.exports = (
     <Route path="/" component={Main}>
       <Route path="/signupdev" component={Signupdev} />
       <Route path="/signuporg" component={Signuporg} />
+      <Route path="/termsofuse" component={Termsofuse} />
+      <Route path="/privacypolicy" component={Privacypolicy} />
+      <Route path="/dashboard" component={Dashboard} />
     </Route>
   </Router>
 );
