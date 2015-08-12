f5785c8e7045db4bd1f93a374c1f034c0e736369
eatpie75 <eatpie75@gmail.com>
2015-08-12 15:57:41 -0700

    [test] Update routes/organization

--- test/routes/organization.js
+++ test/routes/organization.js
@@ -1,31 +1,31 @@
-var chai     = require('chai');
-var expect   = require('chai').expect;
-var chaiHttp = require('chai-http');
-var orgs     = require('../routes/organizations');
-var app      = require('../app');
+var chai          = require('chai');
+var expect        = require('chai').expect;
+var chaiHttp      = require('chai-http');
+var organization  = require('../../routes/organization');
+var app           = require('../../app');
 
 chai.use(chaiHttp);
 
 describe('Organization Route Tests', function() {
 
-    it('should return with status code 200 after POST to /orgs/add', function(done) {
+    it('should return with status code 200 after POST to /organization/add', function(done) {
       chai.request(app)
-        .post('/orgs/add')
+        .post('/organization/add')
         .send({'Test': 'test'})
         .end(function(res){
-          expect(res.req.method).to.equal('POST')
-          expect(res).to.have.status(200)
+          expect(res.req.method).to.equal('POST');
+          expect(res).to.have.status(200);
           done();
-        })
+        });
     });
 
-    it('should return with status code 404 after GET to /orgs/add', function(done) {
+    it('should return with status code 404 after GET to /organization/add', function(done) {
       chai.request(app)
-        .get('/orgs/add')
+        .get('/organization/add')
         .end(function(res){
-          expect(res.res.body.status).to.equal(404)
+          expect(res.res.body.status).to.equal(404);
           done();
-        })
+        });
     });
 
     it('should use delete method on /remove', function(done) {
@@ -33,13 +33,13 @@ describe('Organization Route Tests', function() {
       //   return Math.floor(Math.random(100000) * 100000).toString();
       // };
       chai.request(app)
-        .delete('/orgs/remove')
+        .delete('/organization/remove')
         .send({'Test': 'text'})
         .then(function(res){
           expect(res.req.method).to.equal('DELETE');
           expect(res).to.have.status(200);
           done();
-        })
+        });
     });
 
     it('should use put method on /update', function(done) {
@@ -47,14 +47,13 @@ describe('Organization Route Tests', function() {
       //   return Math.floor(Math.random(100000) * 100000).toString();
       // };
       chai.request(app)
-        .put('/orgs/update')
+        .put('/organization/update')
         .send({})
         .end(function(err, res){
           expect(res.req.method).to.equal('PUT');
           expect(res).to.have.status(200);
           done();
-        })
+        });
     });
-
 
 });
