e8a74750d2f89e89f6c0e4df045453db4859e3ab
eatpie75 <eatpie75@gmail.com>
2015-08-12 16:05:20 -0700

    [test] Update routes/tag

--- test/routes/tag.js
+++ test/routes/tag.js
@@ -1,31 +1,31 @@
 var chai     = require('chai');
 var expect   = require('chai').expect;
 var chaiHttp = require('chai-http');
-var tags     = require('../routes/tags');
-var app      = require('../app');
+var tag      = require('../../routes/tag');
+var app      = require('../../app');
 
 chai.use(chaiHttp);
 
 describe('Tag Route Tests', function() {
 
-    it('should return with status code 200 after POST to /tags/add', function(done) {
+    it('should return with status code 200 after POST to /tag/add', function(done) {
       chai.request(app)
-        .post('/tags/add')
+        .post('/tag/add')
         .send({'Test': 'test'})
         .end(function(res){
-          expect(res.req.method).to.equal('POST')
-          expect(res).to.have.status(200)
+          expect(res.req.method).to.equal('POST');
+          expect(res).to.have.status(200);
           done();
-        })
+        });
     });
 
-    it('should return with status code 404 after GET to /tags/add', function(done) {
+    it('should return with status code 404 after GET to /tag/add', function(done) {
       chai.request(app)
-        .get('/tags/add')
+        .get('/tag/add')
         .end(function(res){
-          expect(res.res.body.status).to.equal(404)
+          expect(res.res.body.status).to.equal(404);
           done();
-        })
+        });
     });
 
     it('should use delete method on /remove', function(done) {
@@ -33,13 +33,13 @@ describe('Tag Route Tests', function() {
       //   return Math.floor(Math.random(100000) * 100000).toString();
       // };
       chai.request(app)
-        .delete('/tags/remove')
+        .delete('/tag/remove')
         .send({'Test': 'text'})
         .then(function(res){
           expect(res.req.method).to.equal('DELETE');
           expect(res).to.have.status(200);
           done();
-        })
+        });
     });
 
     it('should use put method on /update', function(done) {
@@ -47,14 +47,13 @@ describe('Tag Route Tests', function() {
       //   return Math.floor(Math.random(100000) * 100000).toString();
       // };
       chai.request(app)
-        .put('/tags/update')
+        .put('/tag/update')
         .send({})
         .end(function(err, res){
           expect(res.req.method).to.equal('PUT');
           expect(res).to.have.status(200);
           done();
-        })
+        });
     });
-
 
 });
