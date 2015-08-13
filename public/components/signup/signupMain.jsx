0fb9e61a03a78fc9644a03c754567d6a802e70e0
Yoshio Varney <yoshio.varney@gmail.com>
2015-08-13 10:52:27 -0700

    [Feat] 'npm start' will now run gulp file & node server

--- public/components/signup/signupMain.jsx
+++ public/components/signup/signupMain.jsx
@@ -1,9 +1,11 @@
-var React = require('react');
+var React = require('react/addons');
 var Dev = require('./signupDev');
 var Org = require('./signupOrg');
 var Fetch = require('whatwg-fetch');
 
 module.exports = React.createClass({
+	// see http://facebook.github.io/react/docs/two-way-binding-helpers.html
+	mixins: [React.addons.LinkedStateMixin],
 	getInitialState: function() {
 		return {
 			first_name: '',
@@ -12,7 +14,7 @@ module.exports = React.createClass({
 	    password: '',
 	    confirmedPassword: '',
 	    can_message: false, 
-	    user_kind: '',
+	    user_kind: this.props.type,
 	    terms: false
 		}
 	},
@@ -22,17 +24,17 @@ module.exports = React.createClass({
 				<div>{name}</div>
 		    <form className="form-inline names">
 		    	<div className="form-group">
-	  				<input type="text" ref="firstName" className="form-control firstName" onChange={this.handleChange} placeholder="First Name"/>
-	  				<input type="text" ref="lastName" className="form-control lastName" onChange={this.handleChange} placeholder="Last Name"/>
+	  				<input type="text" ref="firstName" className="form-control firstName" valueLink={this.linkState('first_name')} placeholder="First Name"/>
+	  				<input type="text" ref="lastName" className="form-control lastName" valueLink={this.linkState('last_name')} placeholder="Last Name"/>
 		    	</div>
 		    </form>
 		    <div className="form-group">
-	      	<input type="email" ref="emailAddress" className="form-control emailFill" onChange={this.handleChange} placeholder="Email Address"/>
+	      	<input type="email" ref="emailAddress" className="form-control emailFill" valueLink={this.linkState('email')} placeholder="Email Address"/>
 	      </div>
 	      <form className="form-inline passwords">
 		    	<div className="form-group">
-	  				<input type="password" ref="password" className="form-control password" onChange={this.handleChange} placeholder="Password"/>
-	  				<input type="password" ref="confirmedPassword" className="form-control" onChange={this.handleChange} placeholder="Confirm Password"/>
+	  				<input type="password" ref="password" className="form-control password" valueLink={this.linkState('password')} placeholder="Password"/>
+	  				<input type="password" ref="confirmedPassword" className="form-control" valueLink={this.linkState('confirmedPassword')} placeholder="Confirm Password"/>
 		    	</div>
 		    </form>
 	      <h5 className="signupCentered">Password must be more than 8 characters</h5>
@@ -41,18 +43,22 @@ module.exports = React.createClass({
 		)
 	},
 	handleView: function(){
-		return this.props.type === "dev" ? <Dev change={this.handleSubmit} newEmail={this.settingEmail} terms={this.setTerms} message={this.canMessage}/> : <Org />;
+		// render Dev or Org signup
+		// WHY IS NEWEMAIL BEING PASSED??
+		return this.props.type === "dev" ? 
+		       <Dev submitForm={this.handleSubmit} newEmail={this.settingEmail} terms={this.setTerms} message={this.canMessage} /> : 
+		       <Org submitForm={this.handleSubmit} newEmail={this.settingEmail} terms={this.setTerms} /> ;
 	},
-	handleChange: function(){
-		this.setState({
-			first_name: this.refs.firstName.getDOMNode().value,
-	    last_name: this.refs.lastName.getDOMNode().value,
-	    email: this.refs.emailAddress.getDOMNode().value,
-	    password: this.refs.password.getDOMNode().value,
-	    confirmedPassword: this.refs.confirmedPassword.getDOMNode().value,
-	    user_kind: this.props.type
-		})
-	},
+	// handleChange: function(event){
+	// 	this.setState({
+	// 		first_name: event.target.value,
+	//     last_name: this.refs.lastName.getDOMNode().value,
+	//     email: this.refs.emailAddress.getDOMNode().value,
+	//     password: this.refs.password.getDOMNode().value,
+	//     confirmedPassword: this.refs.confirmedPassword.getDOMNode().value,
+	//     user_kind: this.props.type
+	// 	})
+	// },
 	setTerms: function(){
 		this.setState({
 			terms: !this.state.terms
@@ -67,6 +73,7 @@ module.exports = React.createClass({
 		{this.props.newID(newID)}
 	},
 	handleSubmit: function(comment) {
+		console.log("here's state", this.state)
 		var that = this;
 		fetch(this.props.url, {
 			method: 'post',
@@ -83,6 +90,7 @@ module.exports = React.createClass({
 		})
 		.then(function(data) {
 			// call method with id returned from db
+			console.log(data)
 			that.settingEmail(data.id)
 		})
 		.catch(function(error) {
