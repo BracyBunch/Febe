c9680f72b73fa0d0e35722f4c386b1528cc38a7b
eatpie75 <eatpie75@gmail.com>
2015-08-14 12:29:35 -0700

    [fix] Make fetch send cookies

--- public/utils/profile-api.jsx
+++ public/utils/profile-api.jsx
@@ -2,9 +2,9 @@ var Fetch = require('whatwg-fetch');
 
 module.exports = {
   get: function(url){
-    return fetch(url)
-    .then(function(response){
-      return response.json()
-    })
+    return fetch(url, {'credentials': 'same-origin'})
+    .then(function(response){;
+      return response.json();
+    });
   }
 };
