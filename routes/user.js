103f3bb5d6e54617a0e7ff9ae52272979a5fe295
eatpie75 <eatpie75@gmail.com>
2015-08-13 19:22:35 -0700

    [feat] Add PUT /user

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,12 @@
+var _ = require('lodash');
+var Promise = require('bluebird');
 var User = require('../db').User;
 var express = require('express');
-var http = require('http');
 var router = express.Router();
+var validator = require('validator');
 
-// home route
-router.get('/', function(req, res){
-  // access DB to retrieve all users
-  res.send("Users");
-});
+var bcrypt = require('bcrypt');
+var hash = Promise.promisify(bcrypt.hash, bcrypt);
 
 router.get('/:id', function(req, res) {
   var id = Number(req.params.id);
@@ -20,26 +19,46 @@ router.get('/:id', function(req, res) {
   });
 });
 
-router.post('/add', function(req, res){
-  if (req.body.Test === 'test'){
-    return res.send("Test done...");
-  }
-  // create test user
-  User.create(req.body).then(function(user){
-    console.log("User added");
-  });
-  // access DB to add a new user
-  res.send('New User Added');
-});
-
 router.delete('/remove', function(req, res){
   // access DB to remove a user
   res.send('New User Added');
 });
 
-router.put('/update', function(req, res){
-  // access DB to update a user
-  res.send('New User Added');
+router.put('/', function(req, res){
+  if (!req.isAuthenticated()) return res.status(403).send();
+
+  var async = {};
+
+  var editable_fields = [
+    'kind', 'first_name', 'last_name', 'email', 'password',
+    'can_message', 'title', 'bio', 'location', 'links'
+  ];
+
+  var fields = _.pick(req.body, editable_fields);
+
+  if ('password' in fields) {
+    async.password = hash(fields.password, 10).then(function(encrypted) {
+      return encrypted;
+    });
+  }
+
+  if ('email' in fields) {
+    if (validator.isEmail(fields.email) && fields.email !== req.user.email) {
+      async.email = User.check_if_exists(fields.email).then(function(exists) {
+        return (!exists) ? fields.email : req.user.email;
+      });
+    } else {
+      delete fields.email;
+    }
+  }
+
+  Promise.props(async).then(function(slow_fields) {
+    if ('password' in slow_fields) fields.password = slow_fields.password;
+    if ('email' in slow_fields) fields.email = slow_fields.email;
+    User.update(req.user.id, fields).then(function() {
+      res.send();
+    });
+  });
 });
 
 
