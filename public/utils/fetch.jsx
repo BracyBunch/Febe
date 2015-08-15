27cc3b9dd13a704355cc10a052b8be57e7b60cc2
Ryan Jones <ryan.j.jones10@gmail.com>
2015-08-14 19:12:07 -0700

    [fix] Deleting user's  session from the database when logout

--- public/utils/fetch.jsx
+++ public/utils/fetch.jsx
@@ -2,13 +2,10 @@ var _ = require('lodash');
 
 module.exports = function(url, options) {
   options = _.extend(options || {}, {
-    'credentials': 'same-origin',
-    'headers': {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
+    'credentials': 'same-origin'
   });
+  if (options.body) _.extend(options, {'headers': { 'Accept': 'application/json', 'Content-Type': 'application/json'}});
   return fetch(url, options).then(function(response) {
-    return response.json();
+    return (options.body) ? response.json() : response;
   });
 };
