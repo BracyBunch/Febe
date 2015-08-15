f02368d574adfb528a89b16dcc4a9db787ca96c5
James Maveety <jimbobthedrummer@gmail.com>
2015-08-15 12:29:34 -0700

    [feat] Work on org and project views

--- public/views/org-view.jsx
+++ public/views/org-view.jsx
@@ -4,14 +4,26 @@ var Footer = require('../components/shared/footer');
 var Router = require('react-router');
 var Link = Router.Link;
 
+var Participant = require('../components/profile/participant')
+var Timeline = require('../components/profile/org-link')
+var Description = require('../components/profile/org-link')
+var ProjectTags = require('../components/profile/org-link')
+var Contributors = require('../components/profile/org-link')
+var ProjectMedia = require('../components/profile/org-link')
+
 module.exports = React.createClass({
   render: function(){
     return (
       <div>
         <Header link='/' title='Browse'/>
-        <div>Dashboard</div>
-        <Link to="/devprofile"><button className="btn btn-primary">Dev Profile Temp</button></Link>
-        <Link to="/npprofile"><button className="btn btn-primary">Np Profile Temp</button></Link>
+          <Participant />
+          <Participant />
+          <button className='btn btn-warning'> Organization Link </button>
+          <Timeline />
+          <Description />
+          <ProjectTags />
+          <Contributors />
+          <ProjectMedia />
         <Footer />
       </div>
     )
