2c827e3b622e3de07643b2f86deecbea2b262570
Yoshio Varney <yoshio.varney@gmail.com>
2015-08-17 16:03:41 -0700

    [refactor] Converted header.jsx from ES6 to ES5

--- public/components/shared/header.jsx
+++ public/components/shared/header.jsx
@@ -8,55 +8,31 @@ var SigninModal = require('../homepage/signinModal');
 //renderable component
 var Link = Router.Link;
 
-// let React = require('react');
-// let mui = require('mui');
-// let ThemeManager = new mui.Styles.ThemeManager();
+module.exports = React.createClass({
+  childContextTypes: {
+    muiTheme: React.PropTypes.object
+  },
 
-class Header extends React.Component {
-  getChildContext() { 
+  getChildContext: function() {
+    // 
     return {
       muiTheme: ThemeManager.getCurrentTheme()
-    };
-  }
+    }
+  },
 
-  render() {
+  render: function() {
     return (
       <div>
-        <AppBar>
+        <AppBar
+          title="Good In This World"
+          showMenuIconButton={false}
+        >
           <Link to={this.props.link} className="navbar-brand">{this.props.title}</Link>
           <Link to={this.props.link2} className="navbar-brand">{this.props.title2}</Link>
           <Link to={this.props.link3} className="navbar-brand">{this.props.title3}</Link> 
           <SigninModal />     
         </AppBar>
       </div>
-    )
+    );
   }
-};
-
-// Important!
-Header.childContextTypes = {
-  muiTheme: React.PropTypes.object
-};
-
-module.exports = Header;
-
-// module.exports = React.createClass({
-//   render: function() {
-//     return (
-//       <div className="header">
-//         <nav className="navbar navbar-default">
-//           <div className= "container-fluid">
-//             <Link to={this.props.link} className="navbar-brand">{this.props.title}</Link>
-//             <Link to={this.props.link2} className="navbar-brand">{this.props.title2}</Link>
-//             <Link to={this.props.link3} className="navbar-brand">{this.props.title3}</Link>
-//             <ul className="nav navbar-nav navbar-right">
-//               <div id="login">
-//                 <SigninModal/>
-//               </div>
-//             </ul>
-//           </div>
-//         </nav>
-//       </div>
-//     );
-//   }
-// });
+});
