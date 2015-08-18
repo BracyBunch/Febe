212ea6b85f5862f669e47972f190484310ec8745
Ryan Jones <ryan.j.jones10@gmail.com>
2015-08-17 19:38:03 -0700

    [feat] Update to Logo and background

--- public/views/dashboard-view.jsx
+++ public/views/dashboard-view.jsx
@@ -1,20 +1,29 @@
 var React = require('react');
-var Header = require('../components/shared/header');
-var Footer = require('../components/shared/footer');
 var Router = require('react-router');
 var Link = Router.Link;
+var Header = require('../components/shared/header');
+var Footer = require('../components/shared/footer');
+var ThumbnailList = require('../components/thumbnail/thumbnailList');
 
 module.exports = React.createClass({
+  
   render: function(){
     return (
       <div>
         <Header link='/' title='Browse'/>
-        <div>Dashboard</div>
-        <Link to="/devprofile"><button className="btn btn-primary">Dev Profile Temp</button></Link>
-        <Link to="/createproject"><button className="btn btn-primary">Create Project Temp</button></Link>
-        <Link to="/npprofile"><button className="btn btn-primary">Np Profile Temp</button></Link>
-        <Footer />
+        Dashboard
+        <div>
+          <Link to="/devprofile"><button className="btn btn-primary">Dev Profile Temp</button></Link>
+          <Link to="/createproject"><button className="btn btn-primary">Create Project Temp</button></Link>
+          <Link to="/npprofile"><button className="btn btn-primary">Np Profile Temp</button></Link>
+        </div>
+        <div>
+          <ThumbnailList />
+        </div>  
+        
       </div>
     )
   },
 });
+
+//<Footer />
