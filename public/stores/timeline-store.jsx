9f105a805dbe1c7e9c4978650404a7f9cc975b09
eatpie75 <eatpie75@gmail.com>
2015-08-18 15:07:00 -0700

    [fix] Prettify timeline-store

--- public/stores/timeline-store.jsx
+++ public/stores/timeline-store.jsx
@@ -5,14 +5,12 @@ var ajax = require('../utils/fetch');
 module.exports = Reflux.createStore({
   listenables: [Actions],
   getTimeline: function() {
-    return ajax('/user/timeline')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        this.timelineData = data;
-        this.triggerChange();
-      }.bind(this));
+    return ajax('/user/timeline').then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      this.timelineData = data;
+      this.triggerChange();
+    }.bind(this));
   },
   triggerChange: function(){
     this.trigger('change', this.timelineData);
