2459d260c4766d40cbfc219a48dfb5192eab401e
eatpie75 <eatpie75@gmail.com>
2015-08-19 15:34:08 -0700

    [fix] More Travis tests

--- fixtures/import.js
+++ fixtures/import.js
@@ -5,10 +5,11 @@ var causes = require('./causes.json');
 
 var queries = [];
 
-if (process.env.TRAVIS) process.exit();
-
-db.Tag.where({'name': skills[0].name}).then(function(rows) {
-  if (rows.length) process.exit();
+queries.push(db.Tag.where({'name': skills[0].name}).then(function(rows) {
+  if (rows.length || process.env.TRAVIS){
+    console.log('Not installing fixtures');
+    process.exit();
+  }
 
   skills.forEach(function(t) {
     queries.push(db.Tag.save({name: t.name, kind: 'skill'}));
@@ -17,6 +18,6 @@ db.Tag.where({'name': skills[0].name}).then(function(rows) {
   causes.forEach(function(t) {
     queries.push(db.Tag.save({name: t.name, kind: 'cause'}));
   });
-});
+}));
 
 Promise.all(queries).then(function(){console.log('done');});
