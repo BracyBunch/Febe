1591484643088e5c67572838494d8a461cfa1c90
eatpie75 <eatpie75@gmail.com>
2015-08-20 15:12:01 -0700

    [test] Update /user tests

--- test/routes/user.js
+++ test/routes/user.js
@@ -1,53 +1,60 @@
-var chai     = require('chai');
-var expect   = require('chai').expect;
+var Promise = require('bluebird');
+var chai = require('chai');
 var chaiHttp = require('chai-http');
-var user     = require('../../routes/user');
-var app      = require('../../app');
-
+var expect = chai.expect;
 chai.use(chaiHttp);
+chai.request.addPromises(Promise);
+
+var app = require('../../app');
+var models = require('../../db');
+
+var http = chai.request.agent(app);
+
+var clean_up = function(ids, cb) {
+  models.db.query('MATCH (n) WHERE id(n) IN {ids} OPTIONAL MATCH (n)-[r]-() OPTIONAL MATCH (te:TimelineEntry)<-[r2]->(n) DELETE n,r,r2,te', {'ids': ids}, function() {
+    cb();
+  }, function() {
+    cb();
+  });
+};
 
 describe('User Route Tests', function() {
+  var ids_to_be_deleted = [];
+  var instances = {};
 
-    it('should return with status code 200 after POST to /user/add', function(done) {
-      chai.request(app)
-        .post('/user/add')
-        .send({'Test': 'test'})
-        .end(function(res){
-          expect(res.req.method).to.equal('POST');
-          expect(res).to.have.status(200);
-          done();
-        });
+  before(function(done) {
+    models.User.create({'kind': 'rep', 'first_name': 'test', 'last_name': 'user', 'email': 'p_test_rep@gmail.com', 'password': '$2a$10$rRuOCaxfc4p16.cjoAYI2els/JeZvqB9kb707zNcWpFB6ZlP1yzYe'}).then(function(user) {
+      instances.user = user;
+      ids_to_be_deleted.push(instances.user.id);
+      return http.post('/auth/login').send({'email': 'p_test_rep@gmail.com', 'password': 'testtest'});
+    }).then(function() {
+      done();
     });
+  });
 
-    it('should return with status code 404 after GET to /user/add', function(done) {
-      chai.request(app)
-        .get('/user/add')
-        .end(function(res){
-          expect(res.res.body.status).to.equal(404);
-          done();
-        });
-    });
+  after(function(done) {
+    clean_up(ids_to_be_deleted, done);
+  });
 
-    it('should use delete method on /remove', function(done) {
-      chai.request(app)
-        .delete('/user/remove')
-        .send({'Test': 'text'})
-        .then(function(res){
-          expect(res.req.method).to.equal('DELETE');
-          expect(res).to.have.status(200);
-          done();
-        });
+  it('should be able to get User info', function(done) {
+    http.get('/user/' + instances.user.id).send().then(function(res) {
+      expect(res).to.be.json;
+      expect(res.body.first_name).to.be.eql('test');
+      done();
     });
+  });
 
-    it('should use put method on /update', function(done) {
-      chai.request(app)
-        .put('/user/update')
-        .send({'Test': 'text'})
-        .end(function(err, res){
-          expect(res.req.method).to.equal('PUT');
-          expect(res).to.have.status(200);
-          done();
-        });
+  it('should be able to update a User', function(done) {
+    http.put('/user')
+    .send({
+      'first_name': 'updated_test'
+    }).then(function(res) {
+      expect(res).to.have.status(200);
+      return models.User.read(instances.user.id);
+    }).then(function(user) {
+      expect(user.first_name).to.be.eql('updated_test');
+      done();
     });
+  });
 
 });
