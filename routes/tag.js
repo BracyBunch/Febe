7c70c99e89dbb45d2722890d268d0ac9dd9019b0
eatpie75 <eatpie75@gmail.com>
2015-08-20 16:22:29 -0700

    [feat] Add ability to set min_chars to 0 and fetch options on mount in Autocomplete

--- routes/tag.js
+++ routes/tag.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/search', function(req, res) {
   var fragment = req.query.fragment;
   var kind = req.query.kind || 'skill';
-  if (!fragment || fragment.length < 3) return res.status(400).send();
+  // if (!fragment || fragment.length < 3) return res.status(400).send();
 
   Tag.find_by_fragment(fragment, kind).then(function(tags) {
     res.json(tags);
