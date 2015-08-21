40779a3d12ff0792f58f9cee59313c33b038f427
eatpie75 <eatpie75@gmail.com>
2015-08-21 15:13:07 -0700

    [fix] Clean up profile-view

--- public/views/profile-view.jsx
+++ public/views/profile-view.jsx
@@ -8,45 +8,44 @@ var Paper = mui.Paper;
 var Header = require('../components/shared/header');
 var Footer = require('../components/shared/footer');
 var ProfileHeader = require('../components/profile/profile-header');
-var Bio = require('../components/profile/profile-bio');
 var Projects = require('../components/profile/profile-projects');
 var ProfileStore = require('../stores/profile-store');
 var Actions = require('../actions');
 var ProfileHeaderEdit = require('../components/profile/edit-components/profile-header-edit');
-var BioEdit = require('../components/profile/edit-components/profile-bio-edit');
 var ProfileMethods = require('../components/profile/sharedProfileMethods');
 var Autocomplete =require('../components/shared/autocomplete');
 
-module.exports = React.createClass({
+var ProfileView = React.createClass({
   mixins: [
     Reflux.listenTo(ProfileStore, 'onChange')
   ],
   childContextTypes: {
     muiTheme: React.PropTypes.object
   },
-  getChildContext: function(){ 
+  getChildContext: function() {
     return {
       muiTheme: ThemeManager.getCurrentTheme()
     };
   },
-	getInitialState: function(){
-		return {
-			title: '',
-			location: '',
-			bio: '',
-			links: [],
-			strengths: [],
-			interests: [],
-			userData: [],
-			swap: true,
+  getInitialState: function() {
+    return {
+      first_name: '',
+      last_name: '',
+      title: '',
+      location: '',
+      bio: '',
+      links: [],
+      strengths: [],
+      interests: [],
+      userData: {},
+      editting: false,
       hasOrg: false
-		};
-	},
-	componentWillMount: function(){
-		Actions.getProfile(window.localStorage.getItem('userId'));
-	},
-	onChange: function(event, userData){
-		// this.setState({userData: userData});
+    };
+  },
+  componentWillMount: function() {
+    return Actions.getProfile(window.localStorage.getItem('userId'));
+  },
+  onChange: function(event, userData) {
     this.setState({
       kind: userData.kind,
       first_name: userData.first_name,
@@ -58,15 +57,15 @@ module.exports = React.createClass({
       strengths: userData.strengths,
       interests: userData.interests
     });
-	},
+  },
 
-	edit: function() {
+  edit_toggle: function() {
     this.setState({
-    	swap: !this.state.swap
+      editting: !this.state.editting
     });
-	},
+  },
 
-	save: function() {
+  save: function() {
     var updateData = {
       title: this.state.title,
       location: this.state.location,
@@ -80,45 +79,35 @@ module.exports = React.createClass({
         'interests': this.refs.interests.get_selections_array()
       });
     }
-    this.edit();
+    this.edit_toggle();
     ProfileMethods.updateProfile('/user', updateData);
-	},
+  },
 
-	updateTitle: function(title) {
-		this.setState({
-      title: title
-		});
-	},
+  updateHeader: function(state) {
+    this.setState(state);
+  },
 
-	updateLocation: function(location) {
-    this.setState({
-    	location: location
-    });
-	},
-
-	updateLinks: function(link) {
+  updateLinks: function(link) {
     var links = this.state.links.slice();
     links.push(link);
     this.setState({
       links: links
     });
-	},
+  },
 
-	updateBio: function(bio) {
-		this.setState({
-			bio: bio
-		});
-	},
+  updateBio: function(event) {
+    this.setState({'bio': event.target.value});
+  },
 
   strengthsList: function() {
     return this.state.strengths.map(function(strength) {
-      return <h4 className="label-inline"> <span className="label label-color">{strength.name}</span> </h4>;
+      return <h4 key={strength.id} className="label-inline"> <span className="label label-color">{strength.name}</span> </h4>;
     });
   },
 
   interestsList: function() {
     return this.state.interests.map(function(interest) {
-      return <h4 className="label-inline"> <span className="label label-color">{interest.name}</span> </h4>;
+      return <h4 key={interest.id} className="label-inline"> <span className="label label-color">{interest.name}</span> </h4>;
     });
   },
 
@@ -130,20 +119,20 @@ module.exports = React.createClass({
           <div>
             <h3>Tech Strengths</h3>
             {(function() {
-              if (this.state.swap) {
-                return this.strengthsList();
-              } else {
+              if (this.state.editting) {
                 return <Autocomplete url='/tag/search?fragment=' placeholder='Search for strengths' values={this.state.strengths} ref='strengths'/>;
+              } else {
+                return this.strengthsList();
               }
             }.bind(this))()}
           </div>
           <div>
             <h3>Interests</h3>
             {(function() {
-              if (this.state.swap) {
-                return this.interestsList();
-              } else {
+              if (this.state.editting) {
                 return <Autocomplete url='/tag/search?kind=cause&fragment=' placeholder='Search for causes' min_chars={0} values={this.state.interests} ref='interests'/>;
+              } else {
+                return this.interestsList();
               }
             }.bind(this))()}
           </div>
@@ -152,54 +141,75 @@ module.exports = React.createClass({
     );
   },
 
-	profileEdit: function(edit) {
-		return edit ? 
-      <div className="container profileMargin">
-      <Paper zDepth={4}>
-        <div className="row">
-          <div className="col-md-8 col-md-offset-1 profileBox">
-            <ProfileHeader 
-                edit={this.edit}
-    		        firstName={this.state.first_name}
-    		        lastName={this.state.last_name}
-    		        avatar={this.state.userData.avatar}
-    		        title={this.state.title}
-    		        location={this.state.location}
-    		        bio={this.state.bio}
-    		        links={this.state.links} />
+  profile: function() {
+    if (this.state.editting) {
+      return (
+        <div className="container profileMargin">
+        <Paper zDepth={4}>
+          <div className="row">
+            <div className="col-md-8 col-md-offset-1 profileBox">
+              <ProfileHeaderEdit
+                  save={this.save}
+                  onChange={this.updateHeader}
+                  first_name={this.state.first_name}
+                  last_name={this.state.last_name}
+                  avatar={this.state.userData.avatar}
+                  title={this.state.title}
+                  location={this.state.location}
+                  links={this.state.links} />
+            </div>
           </div>
           {this.devFields()}
           <div className="row">
             <div className="col-md-8 col-md-offset-1">
-              <Bio bio={this.state.bio} />
-              <Projects />
+              <div>
+                <h3>Bio</h3>
+                <textarea
+                  value={this.state.bio}
+                  onChange={this.updateBio}
+                  placeholder="Tell us about yourself..."
+                  className="form-control bio"
+                  rows="4"
+                  cols="200"
+                  style={{'marginBottom': '20px'}}
+                  ></textarea>
+              </div>
             </div>
           </div>
+        </Paper>
         </div>
-      </Paper>
-      </div>
-      :
-      <div className="container profileMargin">
-      <Paper zDepth={4}>
-        <div className="row">
-          <div className="col-md-8 col-md-offset-1 profileBox">
-            <ProfileHeaderEdit
-                edit={this.save}
-    		        firstName={this.state.userData.first_name}
-    		        lastName={this.state.userData.last_name}
-                updateTitle={this.updateTitle}
-                updateLocation={this.updateLocation} />
+      );
+    } else {
+      return (
+        <div className="container profileMargin">
+        <Paper zDepth={4}>
+          <div className="row">
+            <div className="col-md-8 col-md-offset-1 profileBox">
+              <ProfileHeader
+                  edit_toggle={this.edit_toggle}
+                  first_name={this.state.first_name}
+                  last_name={this.state.last_name}
+                  avatar={this.state.userData.avatar}
+                  title={this.state.title}
+                  location={this.state.location}
+                  links={this.state.links} />
+            </div>
+            {this.devFields()}
+            <div className="row">
+              <div className="col-md-8 col-md-offset-1">
+                <div>
+                  <h3>Bio</h3>
+                  {this.state.bio}
+                </div>
+                <Projects />
+              </div>
+            </div>
           </div>
+        </Paper>
         </div>
-        {this.devFields()}
-        <div className="row">
-          <div className="col-md-8 col-md-offset-1">
-            <BioEdit updateBio={this.updateBio} />
-          </div>
-        </div>
-      </Paper>
-      </div>
-	},
+      );
+    }
+  },
   generateMenu: [
     { type: MenuItem.Types.LINK, payload: '/', text: 'Home'},
     { type: MenuItem.Types.LINK, payload: '#/dashboard', text: 'Dashboard'},
@@ -210,13 +220,15 @@ module.exports = React.createClass({
     { route: '/', text: 'Team'},
     { type: MenuItem.Types.LINK, payload: 'https://github.com/BracyBunch/Febe', text: 'GitHub' }
   ],
-	render: function() {
-		return (
-		<div>
-      <Header color={{"background-color":"#6E7FD5"}} generateMenu = {this.generateMenu} />
-			{this.profileEdit(this.state.swap)}
-      <Footer />	
-		</div>
-		)
-	}
+  render: function() {
+    return (
+      <div>
+        <Header color={{'backgroundColor': '#6E7FD5'}} generateMenu = {this.generateMenu} />
+        {this.profile()}
+        <Footer />
+      </div>
+    );
+  }
 });
+
+module.exports = ProfileView;
