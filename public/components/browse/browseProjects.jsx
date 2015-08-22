01c708b7f7848e57ebf3d395a3743f6be10b5fef
eatpie75 <eatpie75@gmail.com>
2015-08-22 14:30:19 -0700

    [feat] Make a partially functional browse page

--- public/components/browse/browseProjects.jsx
+++ public/components/browse/browseProjects.jsx
@@ -1,18 +1,37 @@
 var React = require('react');
-var ThumbnailList = require('../thumbnail/thumbnailList');
+var Thumbnail = require('../thumbnail/thumbnail');
 var SearchBar = require('./searchbar');
 
-module.exports = React.createClass({
-  render: function(){
+var ajax = require('../../utils/fetch');
+
+var BrowseProjects = React.createClass({
+  getInitialState: function() {
+    return {
+      'projects': []
+    };
+  },
+  on_selection_change: function(selections) {
+    if (!Object.keys(selections).length) return this.setState({'projects': []});
+    ajax('/project/search?tags=' + JSON.stringify(Object.keys(selections).map(Number))).then(function(res) {
+      return res.json();
+    }).then(function(projects) {
+      this.setState({'projects': projects});
+    }.bind(this));
+  },
+  render: function() {
     return (
       <div>
-        <div >
-          <SearchBar />
-        </div>
         <div>
-          <ThumbnailList />
-        </div> 
-      </div> 
-    )
+          <SearchBar on_change={this.on_selection_change} />
+        </div>
+        <div className="eachThumbnail">
+          {this.state.projects.map(function(project) {
+            return <Thumbnail url={'/project/' + project.id} header={project.name} description={project.description} tags={project.skills} imageURL={project.organization.logo_url} />;
+          })}
+        </div>
+      </div>
+    );
   }
-})
+});
+
+module.exports = BrowseProjects;
