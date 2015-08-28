09bf51152f4ea601a2024a33fae50e07c35f6b8d
Ryan Jones <ryan.j.jones10@gmail.com>
2015-08-27 18:23:35 -0700

    [feat] Profile page now has live project data

--- public/components/profile/profile-projects.jsx
+++ public/components/profile/profile-projects.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var ThumbnailList = require('../thumbnail/thumbnailList');
+var Thumbnail = require('../thumbnail/thumbnail');
 
 module.exports = React.createClass({
   render: function() {
@@ -7,12 +7,15 @@ module.exports = React.createClass({
       <div>
         <div className="project-list">
           <h3>Current Projects</h3>
-            <ThumbnailList />
+            <div className="eachThumbnail">
+              {this.props.projects.map(function(project) {
+                return <Thumbnail key={project.id} url={'/project/' + project.id} header={project.name} description={project.description} tags={project.skills}  />;
+              })}
+            </div>
         </div>
-
-        <div className="project-list">
+        <div className="project-list completed-projs">
           <h3>Completed Projects</h3>
-            <ThumbnailList />
+          <div>No projects currently completed</div>  
         </div>
       </div>
     );
